bcb90cbc7245edca7e0f516e839d895767c02d34
Aleyna Aktaş <aleynaaktas627@gmail.com>
2023-12-08 21:00:42 +0300

    Add comment line

--- week-1/homework-8/src/index.js
+++ week-1/homework-8/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './styles.module.css'
 
 export const ButtonComponent = ({ text, type, ...props }) => {
+  //Merge classes
   const buttonClass = `${styles.button} ${styles[type]}`
   return (
     <button className={buttonClass} {...props}>
